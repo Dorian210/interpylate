32f4dac64895f1be69e7e4c6d72ac6e466b37d5f
Bichet Dorian <dbichet@insa-12913>
2023-05-17 18:32:38 +0200

    speed contest against scipy

---
+++ speed_contest.py
@@ -0,0 +1,29 @@
+# %%
+import numpy as np
+from interpylate import NLinearRegularGridInterpolator
+from scipy.interpolate import RegularGridInterpolator
+n = 1000
+nb_evaluation = n**2
+mode = 'float'
+if mode=='int':
+    pix = np.random.randint(256, size=(n, n, n))
+elif mode=='float':
+    pix = np.random.rand(n, n, n)
+
+my_interp = NLinearRegularGridInterpolator(3)
+my_inds = (n - 1)*np.random.rand(3, nb_evaluation)
+%timeit my_interp.evaluate(pix, my_inds)
+del my_interp
+del my_inds
+
+x = np.arange(pix.shape[0])
+y = np.arange(pix.shape[1])
+z = np.arange(pix.shape[2])
+scipy_interp = RegularGridInterpolator((x, y, z), pix, method='linear', bounds_error=True, fill_value=None)
+scipy_inds = (n - 1)*np.random.rand(nb_evaluation, 3)
+%timeit scipy_interp(scipy_inds)
+del x
+del y
+del z
+del scipy_interp
+del scipy_inds
